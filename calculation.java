1a064debc8fbeab4f79a25de334b6f2236101c27
ROHIT BISWAS <79495712+rohitproject123@users.noreply.github.com>
2022-08-09 17:24:04 +0530

    Add files via upload

---
+++ calculation.java
@@ -0,0 +1,16 @@
+class calculation {
+    public static void main(String[] args) {
+     System.out.println("the two number is  ");
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        int mul = a * b;
+        int sub = a - b;
+        int divide = a % b;
+System.out.println(a + "  " + b);
+        System.out.println("Sum is " + sum);
+        System.out.println("multiplication is " + mul);
+        System.out.println("Subtraction is " + sub);
+        System.out.println("division " + divide);
+    }
+}
