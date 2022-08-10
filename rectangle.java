7a29af3b5379eb006d1bc4c2a636707bc3852cbc
ROHIT BISWAS <79495712+rohitproject123@users.noreply.github.com>
2022-08-10 15:34:10 +0530

    Add files via upload

---
+++ rectangle.java
@@ -0,0 +1,12 @@
+class rectangle{
+public static void main(String args[])
+{
+System.out.println("the length and breadth of rectangle is " +5 +" " +10);
+int width=5;
+int height=10;
+int area=width*height;
+int peri=width+height+width+height;
+System.out.println("Area of rectangle="+area);
+System.out.println("Perimeter of rectangle="+peri);
+}
+}
