a34e88be4ddaa5131acfd419eab1fd03a8767693
ROHIT BISWAS <79495712+rohitproject123@users.noreply.github.com>
2022-08-16 18:05:29 +0530

    Add files via upload

---
+++ Operator.java
@@ -0,0 +1,25 @@
+
+public class Operator {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b= 20;
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		 
+		System.out.println(a<b);
+		System.out.println(a>b);
+		System.out.println(a<b&&b>a);
+		System.out.println(a<b||b>a);
+		System.out.println(a==b);
+		System.out.println(a++ + ++b);
+		System.out.println(b++ + ++a);
+		
+	
+	}
+
+}
